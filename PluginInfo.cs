9f7f95ebcfe7a5a9a5a32a87eecd8012121c23e1
Bunnyiscooler <prod.y2kbuns@gmail.com>
2026-02-08 15:56:11 -0500

    Update plugin GUID and name for demo version

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace LibrePad
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.DEMO.gorillatag.DEMO";
+        public const string Name = "DEMO";
+        public const string Version = "3.6.7";
+    }
+}
