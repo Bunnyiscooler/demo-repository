cb9fb76c192003229ce0a91b50284a5264215113
Bunnyiscooler <prod.y2kbuns@gmail.com>
2026-02-08 16:00:07 -0500

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,12 +1,12 @@
 ﻿namespace DEMO
 {
-
-// this is where your plugin info should be
-     
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
     internal class PluginInfo
     {
         public const string GUID = "com.DEMO.gorillatag.DEMO";
         public const string Name = "DEMO";
-        public const string Version = "3.6.7";
+        public const string Version = "1.0.0"; // CHANGE THIS EVERYTIME YOU UPDATE THE VERSION IN THE PROJECT!!
     }
 }
